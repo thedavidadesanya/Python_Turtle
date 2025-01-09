363ca2e1b5934accfbe4790b9bfc956536fb367c
David Adesanya <davidace2004@gmail.com>
2025-01-08 22:07:25 -0500

    Add files via upload

--- Day 18.py
+++ Day 18.py
@@ -1,9 +1,14 @@
-from turtle import Turtle,Screen #import everything by using the asterisk "*"
+#from turtle import Turtle,Screen #import everything by using the asterisk "*"
+import turtle
 import random
 
-tim = Turtle()
+
+turtle.colormode(255)
+tim = turtle.Turtle()
+
 tim.shape("turtle")
 tim.color("red")
+
 
 def direction_change():
     for x in range(4):
@@ -19,7 +24,7 @@ def dashed_lines():
         tim.forward(10)
 
 
-color_list = ["red", "green", "black", "blue", "yellow", "coral"]
+color_list = ["red", "green", "black", "blue", "yellow", "coral", "pink"]
 
 def shape_generator():
     for x in range(3, 10):
@@ -36,10 +41,31 @@ def random_walk():
     for x in range(200):
         tim.forward(30)
         tim.setheading(random.choice(directions))
-        tim.color(random.choice(color_list))
+        #tim.color(random.choice(color_list))
+        tim.pencolor(random_color())
 
 
-random_walk()
 
-screen = Screen()
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+
+    return (r, g, b)
+
+#random_walk()
+
+def spirograph(size_of_gap):
+    tim.speed("fast")
+    for x in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + 10)
+
+
+spirograph(5)
+#print(dir(Turtle))
+
+
+screen = turtle.Screen()
 screen.exitonclick()
