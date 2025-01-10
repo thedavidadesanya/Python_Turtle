831129318b76082f3ca5f1a0ffd3ae4368c80a3b
David Adesanya <davidace2004@gmail.com>
2025-01-09 21:03:34 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,43 +1,40 @@
-###This code will not work in repl.it as there is no access to the colorgram package here.###
-##We talk about this in the video tutorials##
-"""import colorgram
-
-rgb_colors = []
-colors = colorgram.extract('image.jpg', 30)
-for color in colors:
-    r = color.rgb.r
-    g = color.rgb.g
-    b = color.rgb.b
-    new_color = (r, g, b)
-    rgb_colors.append(new_color)
-
-print(rgb_colors)"""
-
 import turtle
+from turtle import Turtle, Screen
 import random
 
-turtle.colormode(255)
-tim = turtle.Turtle()
-color_list = [(202, 164, 110), (240, 245, 241), (236, 239, 243), (149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20), (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35), (145, 178, 149), (14, 98, 70), (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77), (183, 205, 171), (36, 60, 74), (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102), (12, 70, 64), (107, 127, 153), (176, 192, 208), (168, 99, 102)]
+screen = Screen()
+is_race_on = False
+screen.setup(width =500, height =400)
+user_bet = screen.textinput(title="Make your bet!!", prompt="Which turtle will win the race? Enter a color: ")
+print(user_bet)
 
-tim.penup()
-tim.hideturtle()
-tim.setheading(225)
-tim.forward(250)
-tim.setheading(0)
-number_of_dots = 100
-tim.speed("fastest")
+colors = ["red", "orange","yellow","green", "blue", "purple"]
+y_axis = -100 #could also just create a list
+all_turtles = []
+for x in range(0,6):
+    new_turtle = Turtle(shape = "turtle")
+    new_turtle.color(colors[x])
+    new_turtle.penup()
+    new_turtle.goto(x=-250,y=y_axis)
+    y_axis += 30
+    all_turtles.append(new_turtle)
 
-for x in range(1, number_of_dots + 1):
-    tim.dot(20, random.choice(color_list))
-    tim.forward(50)
 
-    if x % 10 == 0:
-        tim.setheading(90)
-        tim.forward(50)
-        tim.setheading(180)
-        tim.forward(500)
-        tim.setheading(0)
 
-screen = turtle.Screen()
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+
+    for t in all_turtles:
+        if t.xcor() > 230:
+            is_race_on = False
+            winning_color = t.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won the game! The winning turtle is {winning_color}")
+            else:
+                print(f"You've lost! The winning turtle is {winning_color}")
+        distance = random.randint(0,10)
+        t.forward(distance)
+
 screen.exitonclick()
